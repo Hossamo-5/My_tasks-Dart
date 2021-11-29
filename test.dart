a03fb61ea8e557c48a121d9472991f6c796b30d9
Hossamo-5 <81790386+Hossamo-5@users.noreply.github.com>
2021-11-29 20:01:45 +0200

    Calc1
    My task

---
+++ test.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  int num_1 = 2;
+  int num1 = 3;
+  int result = num_1 + num1;
+  print("Your result is: $result");
+  print("===========>");
+  double num_ = 10;
+  double num1_ = 5;
+  double result_ = num_ - num1_;
+  print("Your result is: $result_");
+  print("============>");
+  num _num = 10;
+  num _num1 = 7;
+  num _result = _num * _num1;
+  print("Your result is: $_result");
+  print("=============>");
+  num _1num = 10;
+  num _1num1 = 7;
+  num _result1 = _num / _num1;
+  print("Your result is: $_result1");
+  print("==============>");
+}
